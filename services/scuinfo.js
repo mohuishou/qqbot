b0f68ec3eeb8a9df1b063b7e1f0fbc1490c13f18
小明plus <xiaomingplus@qq.com>
2016-02-01 10:44:55 +0800

    for catch

--- services/scuinfo.js
+++ services/scuinfo.js
@@ -1,8 +1,36 @@
 'use strict';
 var request = require('request');
 var scuinfo = {};
+scuinfo.data = [];
+scuinfo.autoLoad = function(time){
 
+    setInterval(function(){
+        var url = 'http://scuinfo.com/api/hot?pageSize=50';
 
+        request(url,function(e,r,b){
+            if(e){
+                return e;
+            }else{
+
+                try{
+                    var data =  JSON.parse(b);
+                }catch(ee){
+                    var data = {
+                        code:2333,
+                        data:[]
+                    }
+                }
+
+                if(data.data.length>0){
+                    scuinfo.data = data.data;
+                    return;
+                }else{
+                    return 'data null';
+                }
+            }
+        });
+    },time);
+};
 
 scuinfo.random = function(from,start){
     
@@ -45,36 +73,44 @@ scuinfo.test = function(cb){
 
 scuinfo.name = 'scuinfo';
 
-scuinfo.keywords = ['scuinfo','s'];
+scuinfo.keywords = ['scuinfo','s','S'];
 
 scuinfo.execute = function(cb){
-    var url = 'http://scuinfo.com/api/hot';
-    request(url,function(e,r,b){
 
-        if(e){
-            cb(null,e.message);
-            return;
-        }else{
+    if(scuinfo.data.length>0){
+        var index = scuinfo.random(0,scuinfo.data.length-1);
+        cb(null,scuinfo.data[index].content+" 原帖网址:"+"http://scuinfo.com/p/"+scuinfo.data[index].id+"/");
+        return;
+    }else{
 
-            try{
-                var data =  JSON.parse(b);
-            }catch(ee){
-                var data = {
-                    code:2333,
-                    data:[]
-                }
-            }
-
-            if(data.data.length>0){
-                var index = scuinfo.random(0,data.data.length-1);
-                cb(null,data.data[index].content+" 原帖网址:"+"http://scuinfo.com/p/"+data.data[index].id+"/");
+        var url = 'http://scuinfo.com/api/hot';
+        request(url,function(e,r,b){
+            if(e){
+                cb(null,e.message);
                 return;
             }else{
-                cb(null,'现在scuinfo.com啥都没有');
-                return;
+                try{
+                    var data =  JSON.parse(b);
+                }catch(ee){
+                    var data = {
+                        code:2333,
+                        data:[]
+                    }
+                }
+
+                if(data.data.length>0){
+                    scuinfo.data = data.data;
+                    scuinfo.autoLoad(60*1000*10);
+                    var index = scuinfo.random(0,data.data.length-1);
+                    cb(null,data.data[index].content+" 原帖网址:"+"http://scuinfo.com/p/"+data.data[index].id+"/");
+                    return;
+                }else{
+                    cb(null,'现在scuinfo.com啥都没有');
+                    return;
+                }
             }
-        }
-    });
+        });
+    }
 };
 
 
