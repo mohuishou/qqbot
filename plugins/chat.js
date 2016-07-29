463b2232500aabc134962963d6e26fc67dc3d3c5
mohuishou <1@lailin.xyz>
2016-07-29 11:06:06 +0800

    【新增】机器人群聊限制，当机器人在一分钟之内回复条数大于10条时候将会自动退出聊天模式 【新增】新闻查询，列车查询，菜谱查询功能

--- plugins/chat.js
+++ plugins/chat.js
@@ -54,6 +54,12 @@
      @param message 原消息对象
      */
 
+    /**
+     * 图灵机器人聊天
+     * @param content
+     * @param message
+     * @param send
+     */
     chat.turing=function (content,message,send) {
         param.info=content;
         param.userid=message.from_uin;
@@ -72,9 +78,40 @@
                 }
 
                 if(data.text.length>0){
-                    chat.text = data.text;
+                    //文本类消息
+                    if(data.code==100000){
+                        chat.text = data.text;
+                    }else if(data.code==200000){
+                        //图片类消息
+                        chat.text=data.text+"\n"+data.url;
+                    }else if(data.code==302000){
+                        //新闻类消息
+                        chat.text=data.text+"\n";
+                        for (var i=0 ;i<data.list.length;i++){
+                            var news=data.list[i];
+                            chat.text+=news.article+"\n";
+                            chat.text+="点击查看详情"+news.detailurl+"\n";
+                            //新闻条数过多会导致发送失败
+                            if(i>2){
+                                break;
+                            }
+                        }
+                    }else if(data.code==308000){
+                        //菜谱类消息
+                        chat.text=data.text+"\n";
+                        for (var i=0 ;i<data.list.length;i++){
+                            var news=data.list[i];
+                            chat.text+=news.name+"\n";
+                            chat.text+="关键词："+news.info+"\n";
+                            chat.text+="点击查看详情"+news.detailurl+"\n";
+                            //条数过多会导致发送失败
+                            if(i>1){
+                                break;
+                            }
+                        }
+                    }
                     send(chat.text);
-                    return;
+                    return 1;
                 }else{
                     return 'data null';
                 }
@@ -82,6 +119,19 @@
                 console.log(data);
             }
         });
+    }
+
+    /**
+     * 定时清零
+     * @param users_group
+     * @param time
+     */
+    chat.group_reset=function (users_group,time) {
+        setInterval(function () {
+            if(users_group.msg_numbers){
+                users_group.msg_numbers=0;
+            }
+        },time);
     }
 
     module.exports = function(content, send, robot, message) {
@@ -132,6 +182,7 @@
                 if (!users[message.group_code]) {
                     users[message.group_code] = {};
                     users[message.group_code].check_on = 0;
+                    users[message.group_code].msg_numbers = 0;
                 }
                 if(message.from_user.nick == '莫，回首'){
                     if (users[message.group_code].check_on != 1 ) {
@@ -165,7 +216,17 @@
                 }
 
                 users[message.group_code].last=new Date().getTime();
+
+                //检测机器人是否刷屏
+                if(users[message.group_code].msg_numbers>10){
+                    send("1分钟内说了太多话机器人有些累了，已退出智能聊天模式");
+                    users[message.group_code].check_on=0;//退出智能聊天
+                    return;
+                }
+
                 chat.turing(content,message,send);
+                users[message.group_code].msg_numbers+=1;
+                chat.group_reset(users[message.group_code],60*1000);//每分钟清零
 
             }
         }
